317c40aa0041989144486b68dff7a61b31c36470
s230334 <gimpo.s230334@ggh.goe.go.kr>
2024-11-19 15:28:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,17 @@ model = joblib.load('linear_regression_model__1_.pkl')
 
 # 2. 모델 설명
 st.title('신용 등급 분류 에이전트')
-col1, col2, col3=st.column(3)
-with col1:
- st.subheader('모델 설명')
- st.write('-')
- st.write('-')
- st.write('-')
- st.write('-')
- st.write('-')
+st.subheader('모델 설명')
+st.write('- 기계학습 알고리즘: 선형회귀')
+st.write('- 학습 데이터 출처: ')
+st.write('- 훈련 데이터: ')
+st.write('- 테스트 데이터: ')
+st.write('- 인공지능 모델 정확도: ')
  
 
 # 3. 데이터시각화
-with col2:
+col1, col2 = st.columns(2)
+with col1:
  st.subheader('')
 
 
